d6d6ba8918ea6ba57b3cd06305cc3d43c95f2e0b
LeonelaRivarola <leonelarivarola6322@gmail.com>
2025-05-15 09:56:01 -0300

    Guardando cambios locales antes de hacer pull

--- intranetAPI/src/app.js
+++ intranetAPI/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { connectToDb } = require("./config/db");
+const { connectToDb } = require("./config/db.js");
 
 const app = express();
 const PORT = 18000;
