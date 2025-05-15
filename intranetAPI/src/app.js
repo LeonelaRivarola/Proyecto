3420524a9849e021dd341fdb7cd287ef9e7c49ed
LeonelaR22 <leonelarivarola6322@gmail.com>
2025-05-15 11:11:08 -0300

    Guardando cambios locales en app.js

--- intranetAPI/src/app.js
+++ intranetAPI/src/app.js
@@ -9,10 +9,10 @@ const PORT = 18001;
 const HOST = "172.16.14.210"; //26 es la otra pc
 
 //Pueba
-connectToAlum().then(pool => {
+connectToGeaSeguridad().then(pool => {
     app.get('/', async (req, res) => {
         try {
-            const result = await pool.request().query('SELECT TOP 5 * FROM dbo.USUARIOS');
+            const result = await pool.request().query('SELECT TOP 5 USU_CODIGO, USU_PASSWORD FROM USUARIOS');
             res.json(result.recordset);
         } catch (error) {
             res.status(500).json({ error: 'Error consultando la base de datos' });
