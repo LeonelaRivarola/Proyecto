5f430a0a2933c0a1b359914d8accd4fe04e3115c
LeonelaRivarola <leonelarivarola6322@gmail.com>
2025-05-23 09:38:23 -0300

    controladores

--- intranetAPI/src/controllers/authController.js
+++ intranetAPI/src/controllers/authController.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');
-// const { findUserByUsername } = require('../models/userModel');
+// Login, logout, registro, autenticación
+// const bcrypt = require('bcrypt');
 const { connectToGeaSeguridad, sql } = require('../config/db');
 const jwt = require('jsonwebtoken');
 
@@ -57,32 +57,4 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Error en el servidor' });
     }
 };
-
-exports.solicitudes = async (req, res) => {
-    try {
-        const pool = await connectToGeaSeguridad();
-const result = await pool.request().query(`
-    SELECT 
-        SOE.SOE_ID,
-        SOE.SOE_FECHA,
-        TOE.TOE_DESCRIPCION AS Tipo,
-        CONCAT(SOE.SOE_APELLIDO, ' ', SOE.SOE_NOMBRE) AS Usuario,
-        ESO.ESO_DESCRIPCION AS Estado,
-        SOE.SOE_UPDATE,
-        SOE.SOE_USUARIO,
-        SOE.SOE_CUIT
-    FROM SOLICITUD_OBRA_ELECTRICA AS SOE
-    INNER JOIN TIPO_OBRA_ELECTRICA AS TOE ON SOE.SOE_TIPO_ID = TOE.TOE_ID
-    INNER JOIN SOLICITUD_ESTADO AS SES ON SES.SES_SOLICITUD_ID = SOE.SOE_ID
-    INNER JOIN ESTADO_SOLICITUD_OBRA AS ESO ON SES.SES_ESTADO_ID = ESO.ESO_ID
-    ORDER BY SOE.SOE_FECHA DESC
-`);
-
-
-        res.json(result.recordset);
-    } catch (err) {
-        console.error('Error al obtener solicitudes:', err);
-        res.status(500).json({ error: 'Error al obtener solicitudes' });
-    }
-}
 
