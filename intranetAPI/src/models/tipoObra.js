246441463e845643af41b1f7ea32f3c6eb0489df
LeonelaR22 <leonelarivarola6322@gmail.com>
2025-05-27 09:08:28 -0300

    ..

--- intranetAPI/src/models/tipoObra.js
+++ intranetAPI/src/models/tipoObra.js
@@ -1,13 +1,13 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db'); // tu conexión Sequelize
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/db'); // tu conexión Sequelize
 
-const TipoObra = sequelize.define('TipoObra', {
-  TOE_ABREVIATURA: { type: DataTypes.STRING, allowNull: false, unique: true },
-  TOE_DESCRIPCION: { type: DataTypes.STRING, allowNull: false },
-  TOE_INTERNO: { type: DataTypes.STRING(1), allowNull: false }, // 'S' o 'N'
-}, {
-  tableName: 'TIPO_OBRA', // nombre real en la DB
-  timestamps: false,
-});
+// const TipoObra = sequelize.define('TipoObra', {
+//   TOE_ABREVIATURA: { type: DataTypes.STRING, allowNull: false, unique: true },
+//   TOE_DESCRIPCION: { type: DataTypes.STRING, allowNull: false },
+//   TOE_INTERNO: { type: DataTypes.STRING(1), allowNull: false }, // 'S' o 'N'
+// }, {
+//   tableName: 'TIPO_OBRA', // nombre real en la DB
+//   timestamps: false,
+// });
 
-module.exports = TipoObra;
+// module.exports = TipoObra;
