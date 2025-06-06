173e6ab419f00274ebdb599c4508fb43cf9df930
roleg <rochileg@hotmail.com>
2025-06-06 09:21:51 -0300

    agregue front  de emails

--- intranet/src/sesion/Ingresar.js
+++ intranet/src/sesion/Ingresar.js
@@ -37,7 +37,6 @@ const Ingresar = () => {
             const response = await axios.post(`${API_URL}/api/login`, { username, password });
 
             localStorage.setItem('token', response.data.token);
-            console.log(response.data.token);
             localStorage.setItem('username', response.data.username)
             navigate('/Home');
 
