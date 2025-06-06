e1d9a3dd9fb6ce1625ccad60889f3efc62cdf19e
roleg <rochileg@hotmail.com>
2025-06-06 10:21:02 -0300

    probando presupuestos

--- intranetAPI/src/models/presupuestoModel.js
+++ intranetAPI/src/models/presupuestoModel.js
@@ -1,7 +1,7 @@
 const { connectToGeaCorpico, sql } = require('../config/db');
 
 exports.getAll = async () => {
-const pool = await connectToAlum();
+const pool = await connectToGeaCorpico();
 const result = await pool.request().query(`
     SELECT SPO_APELLIDO, SPO_NOMBRE, SPO_SOLICITUD
     FROM SOLICITUD_PRESUPUESTO_OBRA
