c1bac2900e7bec98f5a9237d4baf86dab4bd79c3
roleg <rochileg@hotmail.com>
2025-06-06 08:18:31 -0300

    ...

--- intranet/src/tecnica/obrasElectricas/tiposDeObras/EditarTOE.js
+++ intranet/src/tecnica/obrasElectricas/tiposDeObras/EditarTOE.js
@@ -31,7 +31,7 @@ const EditarTOE = () => {
           }
         });
         const data = await res.json();
-        console.log( "datos de tipos de obra" + data);
+        console.log( "datos de tipos de obra" + data.ABREVIATURA);
         setFormData({
           abreviatura: data.abreviatura || '',
           descripcion: data.descripcion || '',
