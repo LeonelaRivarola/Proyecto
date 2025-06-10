bffa5a5d621faafe82c35319fe57de7a066b34f9
roleg <rochileg@hotmail.com>
2025-06-10 08:35:17 -0300

    eliminar toe

--- intranet/src/tecnica/obrasElectricas/tiposDeObras/TiposDeObras.js
+++ intranet/src/tecnica/obrasElectricas/tiposDeObras/TiposDeObras.js
@@ -17,10 +17,15 @@ import { Outlet } from 'react-router-dom';
 import { API_URL } from '../../../config';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ModalEliminarTipoObra from '../../../componentes/modales/ModalEliminarTipoObra';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 const TiposDeObras = () => {
   const [tipoObras, setTipoObras] = useState([]);
+  const [tipoObraE, setTipoObraE] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [mensajeVisible, setMensajeVisible] = useState(false);
+  const [mensajeTexto, setMensajeTexto] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -49,12 +54,53 @@ const TiposDeObras = () => {
   }
 
   const handleEliminarTO = (id) => {
-    //setSolicitudE(id);
-    //setModalOpen(true);
+    setTipoObraE(id);
+    setModalOpen(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    const token = localStorage.getItem('token');
+    try {
+      await fetch(`${API_URL}/tecnica/obrasElectricas/eliminar-tipoObra/${tipoObraE.TOE_ID}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      setMensajeTexto(`Se ha eliminado exitosamente el tipo de Obra ${tipoObraE.TOE_ID}`);
+      setMensajeVisible(true);
+
+      setTimeout(() => {
+        setMensajeVisible(false);
+      }, 4000);
+
+      await fetchSolicitudes();
+
+    } catch (error) {
+      console.error('Error eliminando el tipo de obra:', error);
+    } finally {
+      setModalOpen(false);
+    }
   };
 
   return (
     <Box>
+      {mensajeVisible && (
+        <Alert
+          severity="success"
+          sx={{
+            mb: 2,
+            borderRadius: 2,
+            backgroundColor: '#d0f0c0',
+            color: '#1b5e20',
+            fontWeight: 'bold'
+          }}
+        >
+          {mensajeTexto}
+        </Alert>
+      )}
       <Paper
         elevation={4}
         sx={{
@@ -94,7 +140,7 @@ const TiposDeObras = () => {
           <Table size="small" sx={{ minWidth: '100%', tableLayout: 'auto' }}>
             <TableHead>
               <TableRow>
-                <TableCell sx={{ fontWeight: 'bold', color: '#5f6368', backgroundColor: '#C8E6C9'}}>ABREVIATURA</TableCell>
+                <TableCell sx={{ fontWeight: 'bold', color: '#5f6368', backgroundColor: '#C8E6C9' }}>ABREVIATURA</TableCell>
                 <TableCell sx={{ fontWeight: 'bold', color: '#5f6368', backgroundColor: '#C8E6C9' }}>DESCRIPCIÓN</TableCell>
                 <TableCell sx={{ fontWeight: 'bold', color: '#5f6368', backgroundColor: '#C8E6C9' }}>INTERNO</TableCell>
                 <TableCell sx={{ fontWeight: 'bold', color: '#5f6368', backgroundColor: '#C8E6C9' }}>ACCIONES</TableCell>
@@ -148,6 +194,12 @@ const TiposDeObras = () => {
         </TableContainer>
       </Box>
       <Outlet />
+      <ModalEliminarTipoObra
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onConfirm={handleConfirmDelete}
+        itemName={tipoObraE.TOE_ID}
+      />
     </Box >
   )
 }
