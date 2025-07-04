aeffc310d796a2feb146cc9c91954dc15233e751
roleg <rochileg@hotmail.com>
2025-07-04 10:37:41 -0300

    editar mapa

--- intranet/src/tecnica/interferencias/EditarInterferencia.js
+++ intranet/src/tecnica/interferencias/EditarInterferencia.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { API_URL } from '../../config';
+import MapaInterferencia from './MapaInterferencia';
 import {
   TextField,
   FormControlLabel,
