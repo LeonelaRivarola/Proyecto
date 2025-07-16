d721792dc4f2ecd6c60573dc187ada451cbd525c
roleg <rochileg@hotmail.com>
2025-07-16 11:07:07 -0300

    ...

--- intranet/src/tecnica/interferencias/Asignar.js
+++ intranet/src/tecnica/interferencias/Asignar.js
@@ -124,7 +124,7 @@ const Asignar = () => {
           Asignar Interferencia N° {id}
         </Typography>
       </Paper>
-      <Paper elevation={3} sx={{ padding: 4, maxWidth: fullWidth, margin: 'auto', marginTop: 6 }}>
+      <Paper elevation={3} sx={{ padding: 4, maxWidth: '100%', margin: 'auto', marginTop: 6 }}>
         <Box component="form" onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             {[
